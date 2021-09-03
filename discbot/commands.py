3a949d6002f867b231e9edf335e7cba8d762695f
mhso <mhso@itu.dk>
2021-09-03 19:05:49 +0200

    Unfinished.

--- discbot/commands.py
+++ discbot/commands.py
@@ -1,9 +1,100 @@
+
+    async def get_data_and_respond(
+        self, handler, message, target_name=None,
+        target_all=True, access_level=None, args=None
+    ):
+        try:
+            handler_args = [message]
+            if args is not None:
+                handler_args.extend(args)
+
+            if target_name is not None:
+                target_id = self.get_target_id(
+                    message.author.id, message.guild.id, target_name, target_all
+                )
+                user_is_registered = self.database.user_exists(message.author.id)
+                if not user_is_registered:
+                    if access_level == "all":
+                        await message.channel.send(
+                            "You must be registered to Int-Far:tm: to use this command."
+                        )
+                        return
+                    elif access_level == "self" and target_id == message.author.id:
+                        await message.channel.send(
+                            (
+                                "You must be registered to Int-Far:tm: " +
+                                "to target yourself with this command."
+                            )
+                        )
+                        return
+
+                handler_args.append(target_id)
+            await handler(*handler_args)
+        except InvalidArgument as arg_exception:
+            await message.channel.send(arg_exception.args[0])
+            self.config.log(arg_exception, self.config.log_error)
+        except DBException as db_exception:
+            response = "Something went wrong when querying the database! "
+            response += self.insert_emotes("{emote_fu}")
+            await message.channel.send(response)
+            self.config.log(db_exception, self.config.log_error)
+
+class Target:
+    def __init__(self, value, default="me"):
+        self.value = value
+        self.default = default
+
+class Parameter:
+    def __init__(self, value):
+        pass
+
+    def 
+
 class Command:
-    def __init__(self, command, description, mandatory_params=None, optional_params=None):
+    def __init__(self, command, description, handler, target_all=True, access_level=None, mandatory_params=None, optional_params=None):
         self.cmd = command
         self.desc = description
+        self.handler = handler
+        self.target_all = target_all
+        self.access_level = access_level
         self.mandatory_params = mandatory_params
         self.optional_params = optional_params
+
+    async def handle_command(client, message):
+        handler_args = [message]
+        if args is not None:
+            handler_args.extend(args)
+
+        if target_name is not None:
+            target_id = self.get_target_id(
+                message.author.id, message.guild.id, target_name, target_all
+            )
+            user_is_registered = self.database.user_exists(message.author.id)
+            if not user_is_registered:
+                if access_level == "all":
+                    await message.channel.send(
+                        "You must be registered to Int-Far:tm: to use this command."
+                    )
+                    return
+                elif access_level == "self" and target_id == message.author.id:
+                    await message.channel.send(
+                        (
+                            "You must be registered to Int-Far:tm: " +
+                            "to target yourself with this command."
+                        )
+                    )
+                    return
+
+            handler_args.append(target_id)
+        await handler(*handler_args)
+    except InvalidArgument as arg_exception:
+        await message.channel.send(arg_exception.args[0])
+        self.config.log(arg_exception, self.config.log_error)
+    except DBException as db_exception:
+        response = "Something went wrong when querying the database! "
+        response += self.insert_emotes("{emote_fu}")
+        await message.channel.send(response)
+        self.config.log(db_exception, self.config.log_error)
 
     def format_params_list(self, params):
         if params == "mandatory":
